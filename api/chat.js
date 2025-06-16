410b0c80880409a63fcc553b2a677fd1f4e78de3
chiba-yu94 <chiba.yu94@gmail.com>
2025-06-16 14:37:32 +0900

    Add small comment chat.js
    Where version setting is

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
   try {
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo", //This line shows the version of GPT
       messages: [
         {
           role: "system",
