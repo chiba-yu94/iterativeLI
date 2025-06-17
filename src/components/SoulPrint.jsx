df383cc4c7c744e5f119ab68b51b63c55dcc9c05
chiba-yu94 <chiba.yu94@gmail.com>
2025-06-17 12:25:11 +0900

    Create SoulPrint.jsx

---
+++ src/components/SoulPrint.jsx
@@ -0,0 +1,11 @@
+// src/components/SoulPrint.jsx
+import React from "react";
+
+export default function SoulPrint(props) {
+  return (
+    // Paste your SVG here:
+    <svg viewBox="0 0 1024 1024" {...props}>
+      {/* all your <g id="back1"> ... etc ... */}
+    </svg>
+  );
+}
