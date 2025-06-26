a12f2994a037deb968d946b8b43c24b8ec12f5d4
chiba-yu94 <chiba.yu94@gmail.com>
2025-06-26 11:11:32 +0900

    Update memory.js

--- api/memory.js
+++ api/memory.js
@@ -1,5 +1,5 @@
 // /api/memory.js
-import memory from "../../src/utils/memory";
+import memory from "../src/utils/memory.js";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
